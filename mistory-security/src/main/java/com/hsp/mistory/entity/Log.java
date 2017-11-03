ca83aa23e54a702de6b60de2adb28759b6d50255
hsp <727138269@qq.com>
2017-11-04 01:06:03 +0800

    ADD 新增安全模块框架搭建

---
+++ mistory-security/src/main/java/com/hsp/mistory/entity/Log.java
@@ -0,0 +1,55 @@
+package com.hsp.mistory.entity;
+
+import java.util.Date;
+
+public class Log {
+    private Integer id;
+
+    private Integer type;
+
+    private String content;
+
+    private Integer userId;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ mistory-security/src/main/java/com/hsp/mistory/entity/Menu.java
@@ -0,0 +1,85 @@
+package com.hsp.mistory.entity;
+
+import java.util.Date;
+
+public class Menu {
+    private Integer id;
+
+    private String title;
+
+    private Integer orderNo;
+
+    private String menuKey;
+
+    private String link;
+
+    private Integer parentId;
+
+    private Boolean isUse;
+
+    private Date createTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public Integer getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(Integer orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public String getMenuKey() {
+        return menuKey;
+    }
+
+    public void setMenuKey(String menuKey) {
+        this.menuKey = menuKey == null ? null : menuKey.trim();
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link == null ? null : link.trim();
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public Boolean getIsUse() {
+        return isUse;
+    }
+
+    public void setIsUse(Boolean isUse) {
+        this.isUse = isUse;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
